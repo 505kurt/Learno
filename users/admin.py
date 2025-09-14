40b7efeddd4e3ef1cbe89616d3fc9d9074d8dab1
505kurt <fbmakurt@gmail.com>
2025-09-13 20:20:54 -0400

    feat: custom user and profile models and admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'role')
+    search_fields = ('id', 'username')
+    list_filter = ('role',)
+
+
+class ProfilesAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    search_fields = ('user__username',)
+
+admin.site.register(models.User, UsersAdmin)
+admin.site.register(models.Profile, ProfilesAdmin)
